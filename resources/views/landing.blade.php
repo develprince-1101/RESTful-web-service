3bda6bac2618f8fe8741d0fbef48f9feae7a023a
Prince <rprinceeme@gmail.com>
2020-05-21 04:07:59 +0800

    hllo

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Welcome!</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+    <style>
+        body {
+            background-image: url('Medical-Background-Free.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-3">
+    <div class="container-fluid">
+        <a href="/" class="navbar-brand mr-3">MDGIS</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="#" class="nav-item nav-link active">Home</a>
+                <a href="#" class="nav-item nav-link">About</a>
+                <a href="#" class="nav-item nav-link">Contact</a>
+            </div>
+            <div class="navbar-nav ml-auto">
+                <a href="#" class="nav-item nav-link">Register</a>
+                <a href="#" class="nav-item nav-link">Login</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container container-fluid">
+<center><img class="rounded-circle" src="MGIS800.png" alt="Generic placeholder image" width="200" height="200"></center>
+    <div class="jumbotron">
+
+        <p class="lead">This application will help patients identify the nearest facilities and services from their location that provides the health services they need, that is also based on the cost of the health service they prefer. They will also be able to see the specialist’s details that fit the health services they need.</p>
+        <p><a href="https://www.tutorialrepublic.com" target="_blank" class="btn btn-success btn-lg">Get started</a></p>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-4 col-xl-3">
+        <center><img class="rounded-circle" src="GIS.png" alt="Generic placeholder image" width="140" height="140"></center>
+            <center><h2>GIS</h2></center>
+            <p>A geographic information system (GIS) is a framework for gathering, managing, and analyzing data. Rooted in the science of geography, GIS integrates many types of data. It analyzes spatial location and organizes layers of information into visualizations using maps and 3D scenes. ​With this unique capability, GIS reveals deeper insights into data, such as patterns, relationships, and situations—helping users make smarter decisions.  </p>
+            <p><a href="https://www.tutorialrepublic.com/html-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+        <center><img class="rounded-circle" src="laravel1.png" alt="Generic placeholder image" width="140" height="140"></center>
+            <center><h2>Laravel</h2></center>
+            <p>Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern and based on Symfony. Some of the features of Laravel are a modular packaging system with a dedicated dependency manager, different ways for accessing relational databases, utilities that aid in application deployment and maintenance, and its orientation toward syntactic sugar.</p>
+            <p><a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3">
+        <center><img class="rounded-circle" src="vue1.png" alt="Generic placeholder image" width="140" height="140"></center>
+            <center><h2>Vue.js</h2></center>
+            <p>Vue.js commonly referred to as Vue is an open-source Model–view–viewmodel JavaScript framework for building user interfaces and single-page applications.It was created by Evan You, and is maintained by him and the rest of the active core team members coming from various companies such as Netlify and Netguru.</p>
+            <p><a href="https://www.tutorialrepublic.com/javascript-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+        </div>
+
+    </div>
+    <hr>
+    <footer>
+        <div class="row">
+            <div class="col-md-6">
+                <p>Copyright © 2020 MDGIS</p>
+            </div>
+            <div class="col-md-6 text-md-right">
+                <a href="#" class="text-dark">Terms of Use</a>
+                <span class="text-muted mx-2">|</span>
+                <a href="#" class="text-dark">Privacy Policy</a>
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ resources/views/landing2.blade.php
@@ -0,0 +1,199 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="MGIS800.png">
+
+    <title>Welcome</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/carousel.css" rel="stylesheet">
+
+    <style>
+        body {
+            background-image: url('bg2.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">MDGIS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+          </ul>
+
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign In</button>
+
+        </div>
+      </nav>
+    </header>
+
+    <main role="main">
+
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="locate5.jpg" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Welcome.</h1>
+                <p>This application will help patients identify the nearest facilities and services from their location that provides the health services they need, that is also based on the cost of the health service they prefer. They will also be able to see the specialist’s details that fit the health services they need.</p>
+                <form class="form-inline mt-2 mt-md-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="E-mail" aria-label="Signup">
+                    <button class="btn btn-primary my-2 my-sm-0" type="submit">Sign Up</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="second-slide" src="gis3.jpg" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption">
+                <h3>GIS as an emerging application .</h3>
+                <p>Nowadays, Geographic Information System (GIS) applications have been on the rise throughout the years. GIS allows people to create their own digital map layers to help solve real-world problems. GIS has progressed into a means for data sharing and collaboration, inspiring a vision that is now rapidly becoming a reality a continuous, overlapping, and interoperable GIS database of the world, about virtually all subjects. </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="third-slide" src="healthcare2.jpg" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1>Healthcare.</h1>
+                <p>The World Health Organization (WHO) refers to the Filipino Healthcare System as “fragmented" and that there is a disparity in the quality of healthcare which means that there is an unfair and unequal distribution of health services.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <div class="container marketing">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="GIS.png" alt="Generic placeholder image" width="140" height="140">
+            <h2>GIS</h2>
+            <p>A geographic information system (GIS) is a framework for gathering, managing, and analyzing data. Rooted in the science of geography, GIS integrates many types of data. It analyzes spatial location and organizes layers of information into visualizations using maps and 3D scenes. ​With this unique capability, GIS reveals deeper insights into data, such as patterns, relationships, and situations—helping users make smarter decisions.</p>
+            <p><a class="btn btn-secondary" href="#go1" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="Laravel1.png" alt="Generic placeholder image" width="140" height="140">
+            <h2>Laravel</h2>
+            <p>Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern and based on Symfony. Some of the features of Laravel are a modular packaging system with a dedicated dependency manager, different ways for accessing relational databases, utilities that aid in application deployment and maintenance, and its orientation toward syntactic sugar.</p>
+            <p><a class="btn btn-secondary" href="#go2" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="vue1.png" alt="Generic placeholder image" width="140" height="140">
+            <h2>Vue.js</h2>
+            <p>Vue.js commonly referred to as Vue is an open-source Model–view–viewmodel JavaScript framework for building user interfaces and single-page applications.It was created by Evan You, and is maintained by him and the rest of the active core team members coming from various companies such as Netlify and Netguru.</p>
+            <p><a class="btn btn-secondary" href="#go3" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette" >
+          <div class="col-md-7" id="go1">
+            <h2 class="featurette-heading"> Gis.. <br><span class="text-muted">What is it.</span></h2>
+            <p class="lead">Hundreds of thousands of organizations in virtually every field are using GIS to make maps that communicate, perform analysis, share information, and solve complex problems around the world. This is changing the way the world works.<a href="https://www.esri.com/en-us/what-is-gis/overview" target="_blank">Read more</a></p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="gis1.png" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2" id="go2">
+            <h2 class="featurette-heading">Laravel. <span class="text-muted">Php framework.</span></h2>
+            <p class="lead">Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things..<a href="https://laravel.com/docs/7.x" target="_blank">Read Documentation</a></p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" src="laravel2.png" alt="Generic placeholder image" width=300 height="300">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7" id="go3">
+            <h2 class="featurette-heading">Vue.js. <span class="text-muted">JavaScript Framework.</span></h2>
+            <p class="lead">An incrementally adoptable ecosystem that scales between a library and a full-featured framework..<a href="https://vuejs.org/v2/guide/" target="_blank">Read Guide</a></p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="vue2.png" alt="Generic placeholder image" width="300" height="300">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->
+
+
+      <!-- FOOTER -->
+      <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2020 MDGIS &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </footer>
+
+    </main>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
